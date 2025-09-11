ca31706fb8b3be5abd36186c6c2c33fce73bcf9c
Your Name <you@example.com>
2025-09-11 14:08:01 +0300

    Сообщение к коммиту

--- emu.py
+++ emu.py
@@ -1,1 +1,11 @@
-print("Hello")
+while True:
+    a = input("username@hostname:-$ ")
+    b = a.split()
+    if a == "exit":
+        break
+    if len(b) == 0:
+        continue
+    if b[0] == "ls":
+        print(a)
+    if b[0] == "cd":
+        print(a)
