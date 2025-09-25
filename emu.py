f7fd5567ca8a0a11bb47c6ac3f54a8911608addf
Your Name <you@example.com>
2025-09-25 12:47:50 +0300

    Сообщение к коммиту

--- emu.py
+++ emu.py
@@ -1,11 +1,15 @@
+import shlex
 while True:
-    a = input("username@hostname:-$ ")
-    b = a.split()
+    a = input("vfs@ ")
+    b = shlex.split(a)
     if a == "exit":
         break
     if len(b) == 0:
         continue
     if b[0] == "ls":
-        print(a)
-    if b[0] == "cd":
-        print(a)
+        print(b)
+    elif b[0] == "cd":
+        print(b)
+    else:
+        print(f'{b[0]}: command not found')
+
